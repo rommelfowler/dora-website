f0d7a479950cc27aec17766b44e74dadf760b8fb
Rommel Fowler <info@rommelfowler.com>
2018-02-23 14:35:48 -0800

    init commit

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+$(window).scroll(function(){
+  var yScroll = $(this).scrollTop();
+  $(".cta h1").css({'transform':'translate(0px,' + yScroll/10 + '% )'});
+
+  $(".cta-img").css({
+    'transform':'translate(0px,' + yScroll/20 + '% )',
+    "-webkit-filter": "blur("+yScroll/75+"px)",
+    "-moz-filter": "blur("+yScroll/75+"px)",
+    "filter": "blur("+yScroll/75+"px)"
+  });
+  $(".design").css({'transform':'translate(0px,-' + yScroll/10 + '% )'});
+
+
+});
