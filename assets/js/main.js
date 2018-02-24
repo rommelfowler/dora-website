e49f0817bcbb56e758b1bb78b46a0431b8dd27a9
Rommel Fowler <info@rommelfowler.com>
2018-02-24 10:47:41 -0800

    dora upate

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// This creates the parallax effect
 $(window).scroll(function(){
   var yScroll = $(this).scrollTop();
   $(".cta h1").css({'transform':'translate(0px,' + yScroll/10 + '% )'});
@@ -9,6 +10,22 @@ $(window).scroll(function(){
     "filter": "blur("+yScroll/75+"px)"
   });
   $(".design").css({'transform':'translate(0px,-' + yScroll/20 + '% )'});
+});
 
 
+// handle links with @href started with '#' only
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+    // target element
+    var $id = $(id);
+    if ($id.length === 10) {
+        return;
+    }
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+    // top position relative to the document
+    var pos = $id.offset().top;
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos});
 });
